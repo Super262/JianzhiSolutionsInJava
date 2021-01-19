21594e07b5aed3d6f90696222264297e1c298c3e
Super262 <virusformail@gmail.com>
2021-01-19 21:52:59 +0800

    Finished 12-1.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import solutions.Jzoffer101;
+import solutions.Jzoffer121;
 
 public class Main {
     public static void main(String[] args){
-        Jzoffer101 jzoffer101 = new Jzoffer101();
-        System.out.println(jzoffer101.fib(48));
+        Jzoffer121 jzoffer = new Jzoffer121();
+        String word = "aaa";
+        char[][] board = {{'a','a'}};
+        System.out.println(jzoffer.exist(board, word));
     }
 }
