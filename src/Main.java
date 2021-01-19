08c8009538a9d516950747ab15d0fd7a23cf28cf
Super262 <virusformail@gmail.com>
2021-01-19 15:21:45 +0800

    Finished 10-1.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import solutions.Jzoffer101;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World! ");
+        Jzoffer101 jzoffer101 = new Jzoffer101();
+        System.out.println(jzoffer101.fib(48));
     }
 }
