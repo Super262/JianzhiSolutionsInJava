e0764742e08b7e17e43f0e40dd80e6c50474b9f4
Super262 <virusformail@gmail.com>
2021-01-20 23:19:35 +0800

    Finished 22-1.

---
+++ src/solutions/Jzoffer221.java
@@ -0,0 +1,27 @@
+package solutions;
+
+public class Jzoffer221 {
+     private static class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) {
+             val = x;
+         }
+     }
+     public ListNode getKthFromEnd(ListNode head, int k) {
+         ListNode sliceHead = head;
+         ListNode sliceEnd = sliceHead;
+         while (k > 0 && sliceEnd != null){
+             sliceEnd = sliceEnd.next;
+             --k;
+         }
+         if(k > 0){
+             return null;
+         }
+         while (sliceEnd != null){
+             sliceHead = sliceHead.next;
+             sliceEnd = sliceEnd.next;
+         }
+         return sliceHead;
+     }
+}
