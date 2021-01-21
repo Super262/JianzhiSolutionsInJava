429fce1ce304b06d6131caa3c6ea03c8f031a3ed
Super262 <virusformail@gmail.com>
2021-01-21 23:45:35 +0800

    Finished 34-1.

--- src/solutions/Jzoffer341.java
+++ src/solutions/Jzoffer341.java
@@ -15,18 +15,17 @@ public class Jzoffer341 {
     }
     public List<List<Integer>> pathSum(TreeNode root,int sum) {
         List<List<Integer>> result = new LinkedList<>();
-        if(root == null){
-            return result;
-        }
-        if (root.val == sum && root.left == null && root.right == null){
-            List<Integer> tempResult = new LinkedList<>();
-            tempResult.add(root.val);
-            result.add(tempResult);
-        } else {
-            result.addAll(pathSum(root.right, sum - root.val));
-            result.addAll(pathSum(root.left, sum - root.val));
-            for (List<Integer> path : result) {
-                path.add(0,root.val);
+        if(root != null){
+            if (root.val == sum && root.left == null && root.right == null){
+                List<Integer> tempResult = new LinkedList<>();
+                tempResult.add(root.val);
+                result.add(tempResult);
+            } else {
+                result.addAll(pathSum(root.right, sum - root.val));
+                result.addAll(pathSum(root.left, sum - root.val));
+                for (List<Integer> path : result) {
+                    path.add(0,root.val);
+                }
             }
         }
         return result;
