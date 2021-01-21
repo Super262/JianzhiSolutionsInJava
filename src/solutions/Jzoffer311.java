62c3b47062005d4afdb6d91d179b94d576c303ed
Super262 <virusformail@gmail.com>
2021-01-21 15:00:02 +0800

    Finished 31-1.

--- src/solutions/Jzoffer311.java
+++ src/solutions/Jzoffer311.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 public class Jzoffer311 {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Deque<Integer> stack = new LinkedList<>();
-        int i = 0, j = 0, a = 0, b = 0;
+        int i = 0, j = 0;
         while (j < popped.length){
             if(!stack.isEmpty() && stack.peek() == popped[j]){
                 stack.pop();
