22cc400c7d7587cd06cb915a6a1af0162d20dc86
Super262 <virusformail@gmail.com>
2021-01-21 09:43:40 +0800

    Finished 26-1.

---
+++ src/solutions/Jzoffer261.java
@@ -0,0 +1,33 @@
+package solutions;
+
+public class Jzoffer261 {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSubStructure (final TreeNode A, final TreeNode B) {
+        if (A == null || B == null){
+            return false;
+        }
+        if (A.val == B.val && BIsPartOfA(A, B)){
+            return true;
+        }
+        return isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    private boolean BIsPartOfA(final TreeNode A,final TreeNode B){
+        if (B == null){
+            return true;
+        }
+        if (A == null || A.val != B.val){
+            return false;
+        }
+        return BIsPartOfA(A.left, B.left) && BIsPartOfA(A.right, B.right);
+    }
+}
