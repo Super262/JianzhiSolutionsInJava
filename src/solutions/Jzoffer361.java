4d691625e687dbaa344ecfbe9cd2a4c18232d722
Super262 <virusformail@gmail.com>
2021-01-22 11:18:02 +0800

    Finished 36-1.

---
+++ src/solutions/Jzoffer361.java
@@ -0,0 +1,50 @@
+package solutions;
+
+public class Jzoffer361 {
+
+    Node prev, head;
+
+    private static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    };
+
+    public Node treeToDoublyList(Node root) {
+        if(root == null){
+            return null;
+        }
+        createDoubleList(root);
+        head.left = prev;
+        prev.right = head;
+        return head;
+    }
+
+    private void createDoubleList(final Node root) {
+        if (root == null){
+            return;
+        }
+        createDoubleList(root.left);
+        if(prev != null){
+            prev.right = root;
+        } else {
+            head = root;
+        }
+        root.left = prev;
+        prev = root;
+        createDoubleList(root.right);
+    }
+
+}
