983c0681d32edf36f81fa54ceb5c36e9f82c6e66
Super262 <virusformail@gmail.com>
2021-01-25 00:00:50 +0800

    Finished 45-1.

---
+++ src/solutions/Jzoffer451.java
@@ -0,0 +1,11 @@
+package solutions;
+
+import java.util.Arrays;
+
+public class Jzoffer451 {
+    public String minNumber(int[] nums) {
+        String[] strNums = Arrays.stream(nums).mapToObj(String::valueOf).toArray(String[]::new);
+        Arrays.sort(strNums,(s1, s2) -> (s1 + s2).compareTo(s2 + s1));
+        return String.join("", strNums);
+    }
+}
