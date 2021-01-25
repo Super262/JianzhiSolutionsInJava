c3236e203bfabe7710f370c2bf78d206f4cece70
Super262 <virusformail@gmail.com>
2021-01-25 23:46:50 +0800

    Finished 51-1.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import solutions.Jzoffer491;
 
 public class Main {
     public static void main(String[] args){
-        Jzoffer491 jzoffer = new Jzoffer491();
-        System.out.println(jzoffer.nthUglyNumber(10));
+
     }
 }
