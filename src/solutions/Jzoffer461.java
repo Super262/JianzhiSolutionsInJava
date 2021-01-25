57bfd6a954cf21d7af823d6cb02968ce32cc8081
Super262 <virusformail@gmail.com>
2021-01-25 09:23:06 +0800

    Finished 46-1.

--- src/solutions/Jzoffer461.java
+++ src/solutions/Jzoffer461.java
@@ -6,17 +6,10 @@ public class Jzoffer461 {
         return count(str, 0, str.length() - 1);
     }
     private int count(final String str, final int start, final int end){
-        if(end - start == 0){
+        if(start >= end){
             return 1;
         }
         final int firstDigit = (str.charAt(start) - '0');
-        if(end - start == 1){
-            if(firstDigit > 0 && firstDigit * 10 + (str.charAt(end) - '0') < 26){
-                return 2;
-            } else{
-                return 1;
-            }
-        }
         if(firstDigit > 0 && firstDigit * 10 + (str.charAt(start + 1) - '0') < 26){
             return count(str, start + 1, end) + count(str, start + 2, end);
         } else {
