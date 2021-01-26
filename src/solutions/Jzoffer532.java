64af6298def3d0d844646864cab21039afff69e5
Super262 <virusformail@gmail.com>
2021-01-26 08:47:29 +0800

    Finished 53-2.

---
+++ src/solutions/Jzoffer532.java
@@ -0,0 +1,15 @@
+package solutions;
+
+public class Jzoffer532 {
+    public int missingNumber(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        for(int i = 0; i < nums.length; ++i){
+            if(i != nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
