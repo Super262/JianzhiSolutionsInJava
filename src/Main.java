f67ac660dc4f51fff242c3d68d3fbca09d1784c1
Super262 <virusformail@gmail.com>
2021-01-27 20:42:06 +0800

    Finished 61-1.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import solutions.Jzoffer601;
-
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args){
-        Jzoffer601 jz = new Jzoffer601();
-        System.out.println(Arrays.toString(jz.dicesProbability(2)));
+
     }
 }
