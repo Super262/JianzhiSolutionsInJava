8718fb5572c116dee979e06f1aad73cc39397338
Super262 <virusformail@gmail.com>
2021-01-27 15:48:40 +0800

    Finished 59-1.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
-import solutions.Jzoffer562;
-import solutions.Jzoffer581;
-
 public class Main {
     public static void main(String[] args){
-        Jzoffer581 jzoffer = new Jzoffer581();
-        System.out.println(jzoffer.reverseWords("  hello world!  "));
+
     }
 }
---
+++ src/solutions/Jzoffer591.java
@@ -0,0 +1,45 @@
+package solutions;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Jzoffer591 {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(k == 0 || nums == null || nums.length == 0){
+            return new int[]{};
+        }
+        int[] result = new int[nums.length - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+
+        for(int i = 0; i < k; ++i){
+            addEleToDeque(nums[i], deque);
+        }
+        result[0] = deque.peekFirst();
+
+        for(int i = 1; i < result.length; ++i){
+            if(nums[i - 1] == deque.peekFirst()){
+                deque.removeFirst();
+            }
+            result[i] = addEleToDeque(nums[i + k - 1], deque);
+        }
+
+        return result;
+    }
+
+    private int addEleToDeque(int ele, Deque<Integer> deque){
+        if(deque.isEmpty()){
+            deque.addFirst(ele);
+        } else {
+            if(ele > deque.peekFirst()){
+                deque.clear();
+                deque.addFirst(ele);
+            } else{
+                while(!deque.isEmpty() && deque.peekLast() < ele){
+                    deque.removeLast();
+                }
+                deque.addLast(ele);
+            }
+        }
+        return deque.peekFirst();
+    }
+}
