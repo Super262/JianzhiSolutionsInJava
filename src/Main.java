ec995f41acb12b50665c643781f027f5ffdb7b7a
Super262 <virusformail@gmail.com>
2021-01-27 23:46:54 +0800

    Finished 62-1.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import solutions.Jzoffer621;
 
 public class Main {
     public static void main(String[] args){
-
+        Jzoffer621 jz = new Jzoffer621();
+        System.out.println(jz.lastRemaining(5, 3));
     }
 }
