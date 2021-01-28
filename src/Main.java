0b9ac3d4e628bc54b9ab51140aa83dc6ba6ebeab
Super262 <virusformail@gmail.com>
2021-01-28 11:34:49 +0800

    Finished 64-1.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
-import solutions.Jzoffer621;
 
 public class Main {
     public static void main(String[] args){
-        Jzoffer621 jz = new Jzoffer621();
-        System.out.println(jz.lastRemaining(5, 3));
     }
 }
---
+++ src/solutions/Jzoffer641.java
@@ -0,0 +1,17 @@
+package solutions;
+
+public class Jzoffer641 {
+    public int sumNums(int n) {
+        return quickMultiply(n, n + 1) >> 1;
+    }
+
+    private int quickMultiply(int a, int b){
+        int result = 0;
+        while(b != 0){
+            result += (b & 1) == 1 ? a : 0;
+            a <<= 1;
+            b >>>= 1;
+        }
+        return result;
+    }
+}
