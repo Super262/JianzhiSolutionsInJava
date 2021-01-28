1d2469aec8267b96f2482783254baf78b3cb6157
Super262 <virusformail@gmail.com>
2021-01-28 11:15:16 +0800

    Finished 63-1.

---
+++ src/solutions/Jzoffer631.java
@@ -0,0 +1,18 @@
+package solutions;
+
+public class Jzoffer631 {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length < 2){
+            return 0;
+        }
+        int prevMin = prices[0];
+        int maxValue = 0;
+
+        for(int price : prices){
+            prevMin = Math.min(price, prevMin);
+            maxValue = Math.max(maxValue, price - prevMin);
+        }
+
+        return maxValue;
+    }
+}
