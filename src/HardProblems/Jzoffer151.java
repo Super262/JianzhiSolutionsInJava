e80e9de0a5e2f8a098835cc420714f2f7b2d3c3a
Super262 <virusformail@gmail.com>
2021-02-01 09:24:50 +0800

    Revised 15-1.

---
+++ src/HardProblems/Jzoffer151.java
@@ -0,0 +1,13 @@
+package HardProblems;
+
+public class Jzoffer151 {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int result = 0;
+        while(n != 0){
+            result += (n & 1);
+            n >>>= 1;
+        }
+        return result;
+    }
+}
