fdafdcbb731bad6c0d7db49bed0e9635d4b0235d
Super262 <virusformail@gmail.com>
2021-02-01 15:08:48 +0800

    Revised 52-1.

--- src/NaughtyProblems/Jzoffer521.java
+++ src/NaughtyProblems/Jzoffer521.java
@@ -1,23 +1,22 @@
-package AllSolutions;
+package NaughtyProblems;
 
 public class Jzoffer521 {
-
-    public ListNode getIntersectionNode(ListNode headA,ListNode headB) {
-        if (headA == null || headB == null) {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null){
             return null;
         }
         ListNode current1 = headA;
         ListNode current2 = headB;
-        while (current1 != null || current2 != null) {
-            if (current1 == null) {
+        while(current1 != null || current2 !=null){
+            if(current1 == null){
                 current1 = headB;
             }
-            if (current2 == null) {
+            if(current2 == null){
                 current2 = headA;
             }
-            if (current1 == current2) {
+            if(current1 == current2){
                 return current1;
-            } else {
+            } else{
                 current1 = current1.next;
                 current2 = current2.next;
             }
@@ -28,7 +27,6 @@ public class Jzoffer521 {
     private static class ListNode {
         int val;
         ListNode next;
-
         ListNode(int x) {
             val = x;
             next = null;
