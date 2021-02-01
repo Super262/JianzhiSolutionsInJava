5aed9e69b68db85f71919f38e84437abae58e49a
Super262 <virusformail@gmail.com>
2021-02-01 16:01:06 +0800

    Revised 59-1.

--- src/AllSolutions/Jzoffer591.java
+++ src/AllSolutions/Jzoffer591.java
@@ -1,51 +1,38 @@
 package AllSolutions;
 
-import java.util.Deque;
 import java.util.LinkedList;
 
 public class Jzoffer591 {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(k == 0 || nums == null || nums.length == 0){
-            return new int[]{};
+        if(nums == null || nums.length == 0){
+            return new int[0];
         }
         int[] result = new int[nums.length - k + 1];
-        Deque<Integer> deque = new LinkedList<>();
-
+        int resultTop = 0;
+        LinkedList<Integer> q = new LinkedList<>();
         for(int i = 0; i < k; ++i){
-            addEleToDeque(nums[i], deque);
-        }
-        if(!deque.isEmpty()){
-            result[0] = deque.peekFirst();
-        }
-
-        for(int i = 1; i < result.length; ++i){
-            if(!deque.isEmpty() && nums[i - 1] == deque.peekFirst()){
-                deque.removeFirst();
+            while(!q.isEmpty() && q.peekLast() < nums[i]){
+                q.removeLast();
             }
-            result[i] = addEleToDeque(nums[i + k - 1], deque);
+            q.addLast(nums[i]);
         }
-
+        if(!q.isEmpty() && result.length > 0){
+            result[resultTop++] = q.peekFirst();
+        }
+        int removedNum;
+        for(int i = k; i < nums.length; ++i){
+            removedNum = nums[i - k];
+            if(!q.isEmpty() && removedNum == q.peekFirst()){
+                q.removeFirst();
+            }
+            while(!q.isEmpty() && q.peekLast() < nums[i]){
+                q.removeLast();
+            }
+            q.addLast(nums[i]);
+            if(!q.isEmpty() && result.length > 0){
+                result[resultTop++] = q.peekFirst();
+            }
+        }
         return result;
-    }
-
-    private int addEleToDeque(int ele, Deque<Integer> deque){
-        if(deque.isEmpty()){
-            deque.addFirst(ele);
-        } else {
-            if(ele > deque.peekFirst()){
-                deque.clear();
-                deque.addFirst(ele);
-            } else{
-                while(!deque.isEmpty() && deque.peekLast() < ele){
-                    deque.removeLast();
-                }
-                deque.addLast(ele);
-            }
-        }
-        if(!deque.isEmpty()){
-            return deque.peekFirst();
-        } else {
-            return -1;
-        }
     }
 }
