6c26cd32c46c128e3446adcbf60b37a2ce1eba14
Super262 <virusformail@gmail.com>
2021-02-01 14:06:53 +0800

    Revised 43-1.

---
+++ src/NaughtyProblems/Jzoffer431.java
@@ -0,0 +1,30 @@
+package NaughtyProblems;
+
+public class Jzoffer431 {
+    public int countDigitOne(final int n) {
+        long countOfDigitOne = 0;
+
+        long base = 10;
+        long digit = 1;
+
+        long high = n / base;
+        long cur = n % base;
+        long low = 0;
+
+        while(low != n){
+            if(cur == 0){
+                countOfDigitOne += (high * digit);
+            } else if(cur == 1){
+                countOfDigitOne += high * digit + low + 1;
+            } else {
+                countOfDigitOne += (high + 1) * digit;
+            }
+            base *= 10;
+            digit *= 10;
+            high = n / base;
+            cur = (n % base) / digit;
+            low = n % digit;
+        }
+        return (int) countOfDigitOne;
+    }
+}
